f969d4886461e13adbaf48f3fd2b32bc22d2b041
Gabe Gress <gress.gabriel@gmail.com>
2022-08-05 16:29:36 +0900

    Added parameter minimization with and without noise

--- startjupyterlab.sh
+++ startjupyterlab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jupyter-lab --ip 127.0.0.1 --port 9000 --collaborative --ServerApp.iopub_data_rate_limit=1.0e5 --ExecutePreprocessor.timeout=-1
+jupyter-lab --ip 172.20.21.199 --port 9000 --collaborative --ServerApp.iopub_data_rate_limit=1.0e5 --ExecutePreprocessor.timeout=-1
